798e2d4a36665a2793bc4c119fc0a1687ba6a0a3
Shaoran <shaoran@sakuranohana.org>
2016-11-21 04:38:26 +0100

    adding info for deployment

--- websockets/wsgi.py
+++ websockets/wsgi.py
@@ -9,8 +9,19 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
+import threading
+from websockets.thread import WS
+obj = WS()
+th = threading.Thread(target = obj.listen_and_replay_to_redis)
+th.start()
+
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "websockets.settings")
 
+
 application = get_wsgi_application()
+
+# start this with:
+# wsgi --socket 127.0.0.1:17999 --wsgi-file websockets/wsgi.py --die-on-term --buffer-size=32768 --workers=5 --master --logto2 /tmp/wsgi_django.log --no-threads-wait --enable-threads
--- websockets/wsgi_websocket.py
+++ websockets/wsgi_websocket.py
@@ -8,3 +8,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "websockets.settings")
 
 from ws4redis.uwsgi_runserver import uWSGIWebsocketServer
 application = uWSGIWebsocketServer()
+
+# start this with
+# uwsgi --socket 127.0.0.1:18000 --wsgi-file websockets/wsgi_websocket.py --die-on-term --gevent 1000 --http-websockets --workers=2 --master --logto2 /tmp/wsgi_websocket.log -b 32768 --protocol=http
